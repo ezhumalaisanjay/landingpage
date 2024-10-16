63fb02e67bcda7dbf22f403c21b4538cfec2230d
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2024-10-16 15:19:39 +0530

    add

---
+++ script.js
@@ -0,0 +1,4 @@
+// Enhance any interactivity needed, such as animations, dynamic content loading, etc.
+document.querySelector('.video-placeholder').addEventListener('click', function() {
+    alert("Video will play here!");
+});
